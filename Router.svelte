39e3bf1a24e8dadaf11bfa6a83d8756bc701dee4
ayndqy <59361040+ayndqy@users.noreply.github.com>
2020-07-15 00:14:03 +0300

    New fuctional and logic

--- Router.svelte
+++ Router.svelte
@@ -1,77 +1,171 @@
 <script context="module">
-  import { readable } from 'svelte/store'
+  import { writable } from 'svelte/store'
 
-  // Pathname store
-  export const pathname = readable(window.location.pathname, (set) => {
-    const setPathname = () => {
-      set(window.location.pathname)
-    }
-
-    window.addEventListener('popstate', setPathname)
-
-    return function stop() {
-      window.removeEventListener('popstate', setPathname)
-    }
-  })
-
-  // Hashname store
-  export const hashname = readable(window.location.hash.slice(1), (set) => {
-    const setHashname = () => {
-      set(window.location.hash.slice(1))
-    }
-
-    window.addEventListener('popstate', setHashname)
-
-    return function stop() {
-      window.removeEventListener('popstate', setHashname)
-    }
-  })
-
-  // Params store
-  export const params = readable(
-    new URL(document.location).searchParams,
-    (set) => {
-      const setParams = () => {
-        set(new URL(document.location).searchParams)
-      }
-
-      window.addEventListener('popstate', setParams)
-
-      return function stop() {
-        window.removeEventListener('popstate', setParams)
-      }
-    },
-  )
-
-  // Navigate function
-  export const navigate = (url, replaceState) => {
-    if (replaceState) {
-      history.replaceState(null, document.title, url)
+  // Params to JSON object
+  const paramsToJSON = (search) => {
+    if (search) {
+      return JSON.parse(
+        '{"' +
+          decodeURI(search.substring(1))
+            .replace(/"/g, '\\"')
+            .replace(/&/g, '","')
+            .replace(/=/g, '":"') +
+          '"}'
+      )
     } else {
-      history.pushState(null, document.title, url)
+      return {}
     }
-    window.dispatchEvent(new Event('popstate'))
+  }
+
+  // Path to Array
+  export const pathToArray = (path) => {
+    let pathArray = path.split('/')
+    pathArray.shift()
+    for (let i = 0; i < pathArray.length; i++) pathArray[i] = '/' + pathArray[i]
+    if (pathArray[pathArray.length - 1] !== '/') pathArray = [...pathArray, '/']
+    if (!pathArray.length) pathArray = ['/']
+    return pathArray
+  }
+
+  // Router store
+  export const router = createRouterStore()
+
+  function createRouterStore() {
+    const { subscribe, set } = writable({
+      path: location.pathname,
+      hash: location.hash,
+      params: paramsToJSON(location.search),
+    })
+
+    // onPopState change store values
+    window.onpopstate = () =>
+      set({
+        path: location.pathname,
+        hash: location.hash,
+        params: paramsToJSON(location.search),
+      })
+
+    // onClick link
+    window.onclick = (e) => {
+      // If it's not <a> element
+      if (e.target.nodeName !== 'A') return
+      // If link have 'external' attribute
+      if (e.target.getAttribute('external') === '') return
+      if (e.target.getAttribute('external') === 'true') return
+      // Get href and navigate to it
+      router.navigate(e.target.getAttribute('href'))
+      // Prevent page reload and url change
+      return false
+    }
+
+    return {
+      subscribe,
+
+      navigate: (href, replaceState) => {
+        if (replaceState) {
+          history.replaceState({}, null, href)
+        } else {
+          history.pushState({}, null, href)
+        }
+        window.dispatchEvent(new Event('popstate'))
+      },
+    }
   }
 </script>
 
 <script>
-  export let path = null
-  export let hash = null
-  export let title = document.title
+  import { getContext, setContext, afterUpdate } from 'svelte'
 
-  // Path
-  $: isPathActive = path === null || path === $pathname
+  export let fallback = false
+  export let path = fallback ? null : '/'
+  export let title = null
 
-  // Hash
-  $: isHashActive = hash === null || hash === $hashname
+  let route
+  let routeContext = getContext('route')
+  let depth = routeContext ? routeContext.depth + 1 : -1
+  let activeChildren = []
+  let activateFallback
+  let isFallbackActive
+  let isPathActive
 
-  // Params initialisation
-  $params
+  route = {
+    fallback,
+    path,
+    title,
+    depth,
+    addChild: (path) => {
+      activeChildren = [path, ...activeChildren]
+    },
+    addFallbackActivator: (func) => {
+      activateFallback = func
+    },
+  }
 
-  // Title
-  $: isPathActive && isHashActive ? (document.title = title) : null
+  // Context for child routes
+  setContext('route', route)
+
+  // Errors
+  // If path don't begin from '/'
+  if (route.path && route.path.substring(0, 1) !== '/') {
+    throw new Error(
+      `'${route.path}' is incorrect path. Path must begin from '/'`
+    )
+  }
+  // If fallback Route not in Route component
+  if (route.fallback && route.depth < 0) {
+    throw new Error(`<Route fallback> must be inside <Route> component`)
+  }
+  // If Route component in fallback Route
+  if (routeContext && routeContext.fallback) {
+    throw new Error(`<Route> component can't be inside <Route fallback>`)
+  }
+
+  // Route dynamic properties
+  $: route = { fallback, path, title, depth }
+
+  // On $router.path change reset
+  $: if ($router.path) {
+    activeChildren = []
+    isFallbackActive = false
+  }
+  // Add active child to parent
+  $: if ($router.path && routeContext && !route.fallback && isActive)
+    routeContext.addChild(path)
+  // Аdd fallback activator to parent
+  $: if ($router.path && routeContext && route.fallback)
+    routeContext.addFallbackActivator(() => (isFallbackActive = true))
+
+  // Array with routes in path
+  $: pathArray = pathToArray($router.path)
+
+  // Is fallback can exist (depth)
+  $: isFallbackActive = route.fallback
+    ? route.depth < pathArray.length || pathArray.length === 0
+      ? isFallbackActive
+      : false
+    : false
+
+  // Is Route path equal router path
+  $: isPathActive = !route.fallback
+    ? route.path === pathArray[route.depth] || route.depth < 0
+    : false
+
+  // If Route is active
+  $: isActive = isPathActive || isFallbackActive
+
+  // Title change
+  $: $router.path && route.title && isActive
+    ? (document.title = route.title)
+    : null
+
+  // Activate fallback
+  afterUpdate(() => {
+    Boolean(activateFallback) && activeChildren.length === 0
+      ? activateFallback()
+      : null
+  })
 </script>
 
-{#if isPathActive && isHashActive}
+{#if isActive}
   <slot />
 {/if}
