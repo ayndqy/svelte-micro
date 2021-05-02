f945e0e2b4cbf90f0f20aeeb6b6b99aa0abc2ebc
ayndqy <59361040+ayndqy@users.noreply.github.com>
2021-05-02 17:38:15 +0300

    0.1.14
    Optimized size in bundle
    Migrated to esbuild
    Small code refactoring

---
+++ esbuild.js
@@ -0,0 +1,31 @@
+const esbuild = require('esbuild');
+const sveltePlugin = require('esbuild-svelte');
+const pkg = require('./package.json');
+
+(async () => {
+  await esbuild.build({
+    entryPoints: ['src/index.js'],
+    bundle: true,
+    outfile: pkg.module,
+    format: 'esm',
+    minify: true,
+    external: [
+      'svelte',
+      'svelte/*'
+    ],
+    plugins: [sveltePlugin()]
+  });
+
+  await esbuild.build({
+    entryPoints: ['src/index.js'],
+    bundle: true,
+    outfile: pkg.main,
+    format: 'cjs',
+    minify: true,
+    external: [
+      'svelte',
+      'svelte/*'
+    ],
+    plugins: [sveltePlugin()]
+  });
+})()
--- src/lib/pathToArray.js
+++
@@ -1,9 +0,0 @@
-export const pathToArray = (path) => {
-  let pathArray = path.split('/').filter((path) => path !== '');
-
-  for (let i = 0; i < pathArray.length; i++) {
-    pathArray[i] = '/' + pathArray[i];
-  }
-
-  return pathArray;
-};
