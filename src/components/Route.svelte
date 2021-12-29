6f8eaf03515bef16cf36d627bde555e31f1d9313
ayndqy <ayndqy@gmail.com>
2021-12-29 07:34:56 +0200

    1.5.0
    - Fix circular dependency
    - Code prettify

--- src/components/Route.svelte
+++ src/components/Route.svelte
@@ -1,7 +1,7 @@
 <svelte:options immutable={false} />
 
 <script>
-  import { onDestroy, getContext, setContext, hasContext } from 'svelte'
+  import { onDestroy, getContext, setContext } from 'svelte'
   import { writable } from 'svelte/store'
   import { path as globalPath } from '../lib/stores'
   import getRouteDepth from '../lib/getRouteDepth'
@@ -10,49 +10,27 @@
   export let fallback = false
   export let path = '/'
 
-  const _root = writable(!hasContext('_root'))
-  const _fallback = writable(fallback)
-  const _path = writable(path)
-  const _depth = writable(getRouteDepth(fallback, path, 0))
-  const _children = writable([])
+  const route = writable({})
+  const children = writable([])
 
-  const _contextRoot = getContext('_root')
-  const _contextFallback = getContext('_fallback')
-  const _contextPath = getContext('_path')
-  const _contextDepth = getContext('_depth')
-  const _contextChildren = getContext('_children') ?? writable([])
-  const _contextIndex = $_contextChildren?.length ?? 0
+  const contextRoute = getContext('_route')
+  const contextChildren = getContext('_children') ?? writable([])
+  const contextIndex = $contextChildren?.length
 
-  $: {
-    if ($_path.substring(0, 1) !== '/')
-      throw new Error(`'${$_path}' is invalid path. Route path must start from '/'`)
-    if ($_root && $_fallback === true)
-      throw new Error(`<Route fallback> cannot be outside root <Route>`)
-    if ($_root && $_path !== '/')
-      throw new Error(`<Route path="${path}"> cannot be outside root <Route>`)
-    if (!$_root && $_contextFallback)
-      throw new Error(`Routes cannot be inside <Route fallback>`)
+  $: $route = {
+    root: contextRoute === undefined,
+    fallback: fallback,
+    path: path,
+    depth: getRouteDepth(fallback, path, $contextRoute?.depth),
   }
 
-  $: $_fallback = fallback
-  $: $_path = path
-  $: $_depth = getRouteDepth($_fallback, $_path, $_contextDepth)
+  $: $contextChildren[contextIndex] = $route
+  onDestroy(() => ($contextChildren[contextIndex] = null))
 
-  $: $_contextChildren[_contextIndex] = {
-    root: $_root,
-    fallback: $_fallback,
-    path: $_path,
-    depth: $_depth,
-  }
-  onDestroy(() => ($_contextChildren[_contextIndex] = null))
-
-  setContext('_root', _root)
-  setContext('_fallback', _fallback)
-  setContext('_path', _path)
-  setContext('_depth', _depth)
-  setContext('_children', _children)
+  setContext('_route', route)
+  setContext('_children', children)
 </script>
 
-{#if isRouteActive($globalPath, $_root, $_fallback, $_path, $_depth, $_contextChildren)}
+{#if isRouteActive($globalPath, $route.root, $route.fallback, $route.path, $route.depth, $contextChildren)}
   <slot />
 {/if}
--- src/lib/router.js
+++ src/lib/router.js
@@ -1,18 +1,3 @@
-import linkClickHandler from './linkClickHandler'
-
-export let options = {
-  _reloadPrevent: true,
-  get reloadPrevent() {
-    return this._reloadPrevent
-  },
-  set reloadPrevent(value = this._reloadPrevent) {
-    this._reloadPrevent = value
-    this._reloadPrevent
-      ? window.addEventListener('click', linkClickHandler)
-      : window.removeEventListener('click', linkClickHandler)
-  },
-}
-
 export const router = {
   push: (href = '/') => {
     history.pushState({}, null, href)
@@ -25,4 +10,39 @@ export const router = {
   },
 
   setOptions: (changedOptions = {}) => Object.assign(options, changedOptions),
+}
+
+const linkClickHandler = (event) => {
+  let target = event.target.closest('a[href]')
+  let isTargetInvalid =
+    target === null ||
+    target.nodeName !== 'A' ||
+    target.getAttribute('external-href') === '' ||
+    target.getAttribute('external-href') === 'true' ||
+    target.getAttribute('href').substring(0, 7) === 'http://' ||
+    target.getAttribute('href').substring(0, 8) === 'https://' ||
+    target.getAttribute('href').substring(0, 2) === '//' ||
+    target.getAttribute('href').substring(0, 7) === 'mailto:' ||
+    target.getAttribute('href').substring(0, 4) === 'tel:'
+
+  if (isTargetInvalid) return true
+
+  target?.getAttribute('href') === location.pathname
+    ? router.replace(target?.getAttribute('href'))
+    : router.push(target?.getAttribute('href'))
+
+  event.preventDefault()
+}
+
+export let options = {
+  _reloadPrevent: true,
+  get reloadPrevent() {
+    return this._reloadPrevent
+  },
+  set reloadPrevent(value = this._reloadPrevent) {
+    this._reloadPrevent = value
+    this._reloadPrevent
+      ? window.addEventListener('click', linkClickHandler)
+      : window.removeEventListener('click', linkClickHandler)
+  },
 }
