2a1d5f2f7a080d1441c770e289d82f2d0cc0b3a5
ayndqy <ayndqy@gmail.com>
2023-02-18 22:18:45 +0000

    Migrate to @svelte/package v2

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,6 @@ import { vitePreprocess } from '@sveltejs/kit/vite'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-
-  package: {
-    source: './src',
-    dir: './package',
-    emitTypes: true,
-    exports: (filename) => ['index.ts'].includes(filename),
-  },
 }
 
 export default config
